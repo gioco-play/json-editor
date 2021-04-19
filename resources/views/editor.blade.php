7b5ef79e1213360d50ddc4244963f215bb58593d
Xuan Yang <xuan.yang@goldenf.co>
2021-04-19 14:57:14 +0800

    fix js, css

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -7,7 +7,7 @@
 
         @include('admin::form.error')
 
-        <div id="{{$id}}" style="width: 100%; height: 100%;"></div>
+        <div id="{{$id}}" style="width: 100%; height: {!! $attributes !!};"></div>
 
         <input type="hidden" id="{{$id}}_input" name="{{$name}}" value="{{ old($column, $value) }}" />
         @include('admin::form.help-block')
